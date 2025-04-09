54dbd5694cd6f12b899c58f0061842252fc13566
Shrikant Koli <shrrikantkoli701@gmail.com>
2025-04-09 17:10:40 +0530

    fdsfds

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -6,7 +6,7 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <flutter_pos_printer_platform_image_3_sk_sk/flutter_pos_printer_platform_plugin.h>
+#include <flutter_pos_printer_platform_image_3_sk/flutter_pos_printer_platform_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FlutterPosPrinterPlatformPluginRegisterWithRegistrar(
